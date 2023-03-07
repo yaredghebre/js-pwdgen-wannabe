0bb44ae39e8cecd19054dd60cba42a04c207ffd6
yaredghebre <yared2391@gmail.com>
2023-03-07 12:19:32 -0300

    Add: Project Complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert ("ciao");
+// RACCOLTA DATI UTENTE
+const userName = prompt ("Ciao! Qual'è il tuo nome?")
+const userSurname = prompt ("E il tuo cognome?")
+const userFavColor = prompt ("Qual'è  il tuo colore preferito?")
+
+// ELABORAZIONE
+const userResult = (userName + userSurname + userFavColor + 21)
+console.log(userResult);
+
+// OUTPUT
+document.getElementById('user-password').innerHTML = userResult;
